539b88b51601cf53cdf64b9ed83e74574f17f07b
yezawei <112426848+yezawei@users.noreply.github.com>
2023-04-02 18:54:03 -0500

    Add files via upload

--- ChessMain.py
+++ ChessMain.py
@@ -16,8 +16,7 @@ def loadImages():
     pieces = ['bR', 'bN', 'bB', 'bQ', 'bK',
               'bp', 'wB', 'wN', 'wR', 'wQ', 'wK', 'wp']
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load(
-            "images/" + piece + ".png")(SQ_SIZE, SQ_SIZE))
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
         # access the image by calling 'IMAGES['wp']
 
 """
@@ -26,11 +25,38 @@ Main driver for user input and updating graphics
 
 
 def main():
+    p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
-    print(gs.board)
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            clock.tick(MAX_FPS)
+            p.display.flip()
+            drawGameState(screen, gs)
+'''
+Responsive for all the graphics within a current game state.
+'''
 
 
-main()
+def drawGameState(screen, gs):
+    drawBoard(screen) #draw squares on the board
+    drawPieces(screen, gs.board) #draw pieces on the board
+
+def drawBoard(screen):
+    colors = [p.Color("light gray"), p.Color("dark green")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawPieces(screen, board):
+    pass
+
+if __name__ == '__main__':
+    main()
